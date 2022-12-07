ce127282dd17026096d6dcba7168151796e0bad6
hupe1980 <frankhuebner1980@gmail.com>
2022-12-07 23:32:15 +0100

    Update projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,23 @@
-const { typescript } = require('projen');
+const { javascript, typescript } = require('projen');
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'node-rasp',
   license: 'MIT',
   copyrightOwner: 'Frank Hübner',
   releaseToNpm: true,
+  depsUpgrade: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: 'AUTOMATION_GITHUB_TOKEN',
+      schedule: javascript.UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['hupe1980'],
+  },
   devDeps: ['axios'],
 });
 project.gitignore.exclude('.DS_Store');
